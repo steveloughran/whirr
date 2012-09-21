50aeb767737eda1fee558c5943947216b2dd5250
Steve Loughran <stevel@hortonworks.com>
2012-09-21 14:54:43 +0100

    Initial source files created, derived from the Hama service

---
+++ services/hdp/src/main/java/org/apache/whirr/service/hdp/ambari/AmbariConstants.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.whirr.service.hdp.ambari;
+
+public class AmbariConstants {
+
+  public static final int AMBARI_SERVER_WEB_UI_PORT = 40013;
+
+  public static final String AMBARI_DEFAULT_PROPERTIES = "whirr-ambari-default.properties";
+
+  public static final String KEY_INSTALL_FUNCTION = "whirr.ambari.install-function";
+  public static final String KEY_CONFIGURE_FUNCTION = "whirr.ambari.configure-function";
+  
+  
+  public static final String KEY_TARBALL_URL = "whirr.ambari.tarball.url";
+  public static final String KEY_START_FUNCTION = "whirr.ambari.start-function";
+
+  public static final String FUNCTION_INSTALL = "install_ambari";
+  public static final String FUNCTION_POST_CONFIGURE = "configure_ambari";
+  public static final String FUNCTION_START = "start_ambari";
+
+}
