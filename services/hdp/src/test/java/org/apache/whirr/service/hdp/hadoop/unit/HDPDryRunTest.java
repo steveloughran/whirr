96dff742fbe5aa8253f17c86fa35d6fe96a89652
Steve Loughran <stevel@hortonworks.com>
2012-09-24 15:19:39 +0100

    Move the hdp tests down a level

--- services/hdp/src/test/java/org/apache/whirr/service/hdp/hadoop/unit/HDPDryRunTest.java
+++ services/hdp/src/test/java/org/apache/whirr/service/hdp/hadoop/unit/HDPDryRunTest.java
@@ -16,7 +16,7 @@
  *  limitations under the License.
  */
 
-package org.apache.whirr.service.hdp.unit;
+package org.apache.whirr.service.hdp.hadoop.unit;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
